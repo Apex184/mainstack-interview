abf81886e97a6c15b05d43f6f55fd62c53ce6463
Akindele <akindeleapex@gmail.com>
2024-03-22 12:22:43 +0100

    health

--- src/routes/healthCheckRoute.ts
+++ src/routes/healthCheckRoute.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  return res.status(200).json("Welcome to Roodi Technology Limited! ✍️");
+  return res.status(200).json("Welcome to Main Stack Career! ✍️");
 });
 
 export { router as healthCheckerRouter };
